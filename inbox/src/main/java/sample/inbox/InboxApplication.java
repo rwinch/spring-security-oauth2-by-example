3e4e3acc178a04fb3751a870c4d6c8c5b594eb47
Rob Winch <rwinch@users.noreply.github.com>
2019-08-07 08:50:00 -0500

    Inbox Uses Access Token

--- inbox/src/main/java/sample/inbox/InboxApplication.java
+++ inbox/src/main/java/sample/inbox/InboxApplication.java
@@ -3,14 +3,23 @@ package sample.inbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class InboxApplication {
 
 	@Bean
-	WebClient webClient() {
-		return WebClient.create();
+	WebClient webClient(ClientRegistrationRepository clients,
+			OAuth2AuthorizedClientRepository authz) {
+		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+			new ServletOAuth2AuthorizedClientExchangeFilterFunction(clients, authz);
+		oauth2.setDefaultOAuth2AuthorizedClient(true);
+		return WebClient.builder()
+			.apply(oauth2.oauth2Configuration())
+			.build();
 	}
 
 	public static void main(String[] args) {
